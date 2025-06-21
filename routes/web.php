56ef557e399f781440d2fe84851869ac2d397500
polibee <1244762327@qq.com>
2025-06-21 21:50:05 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 
-Route::get('/',[StaticPagesController::class,'home']);
-Route::get('/help',[StaticPagesController::class,'help']);
-Route::get('/about',[StaticPagesController::class,'about']);
+Route::get('/',[StaticPagesController::class,'home'])->name('home');
+Route::get('/help',[StaticPagesController::class,'help'])->name('help');
+Route::get('/about',[StaticPagesController::class,'about'])->name('about');
