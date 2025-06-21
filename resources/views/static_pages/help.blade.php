e5c65e5b455a6b34336d34e25a3b148b30bcd63b
polibee <1244762327@qq.com>
2025-06-21 14:21:53 +0800

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+<title>Weibo App</title>
+
+    </head>
+    <body>
+        <h1>帮助</h1>
+    </body>
+    </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaticPagesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[StaticPagesController::class,'home']);
+Route::get('/help',[StaticPagesController::class,'help']);
+Route::get('/about',[StaticPagesController::class,'about']);
