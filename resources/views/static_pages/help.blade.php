cef8ee019f9661513c1516e6272dfd82b8f58b0d
polibee <1244762327@qq.com>
2025-06-21 14:34:12 +0800

    模板切分

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,10 +1,5 @@
-<!doctype html>
-<html>
-    <head>
-<title>Weibo App</title>
-
-    </head>
-    <body>
-        <h1>帮助</h1>
-    </body>
-    </html>
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+<h1>帮助页</h1>
+@stop
