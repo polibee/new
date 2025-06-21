7aaa7eebbda6a579bc21a4227a23adaf54d9851f
polibee <1244762327@qq.com>
2025-06-21 21:57:23 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
-
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
+Route::get('signup',[UsersController::class,'create'])->name('signup');
