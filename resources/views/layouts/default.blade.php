f6dc6630ed67cc25bf7a0cc875b911bbdfe4f0ad
polibee <1244762327@qq.com>
2025-06-26 09:35:26 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,5 +13,7 @@
           @include('layouts._footer')
         </div>
       </div>
-  </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
